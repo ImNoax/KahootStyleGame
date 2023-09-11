f97df4f10089a71f0009ef0a9c37704e81500a6c
Sofiane Mouici <sofiane.mouici@polymtl.ca>
2023-09-11 18:06:32 -0400

    Début de la Vue Admin

---
+++ client/src/app/pages/admin-jeu-page/admin-jeu-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-admin-jeu-page',
+    templateUrl: './admin-jeu-page.component.html',
+    styleUrls: ['./admin-jeu-page.component.scss'],
+})
+export class AdminJeuPageComponent {}
