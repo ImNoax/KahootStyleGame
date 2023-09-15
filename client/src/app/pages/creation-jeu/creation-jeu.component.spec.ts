3bfcc16cbdb2a76f8d068429e72f9db6799a45ca
unknown <eug1214@gmail.com>
2023-09-15 11:15:08 -0400

    amelioration vue creation

--- client/src/app/pages/creation-jeu/creation-jeu.component.spec.ts
+++ client/src/app/pages/creation-jeu/creation-jeu.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreationJeuComponent } from './creation-jeu.component';
 
 describe('CreationJeuComponent', () => {
-  let component: CreationJeuComponent;
-  let fixture: ComponentFixture<CreationJeuComponent>;
+    let component: CreationJeuComponent;
+    let fixture: ComponentFixture<CreationJeuComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CreationJeuComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [CreationJeuComponent],
+        });
+        fixture = TestBed.createComponent(CreationJeuComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(CreationJeuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
