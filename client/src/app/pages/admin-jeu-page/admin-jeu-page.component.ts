0b20da89a62e60f38988d635ecb2f3f5aaf172d3
Sofiane Mouici <sofiane.mouici@polymtl.ca>
2023-09-14 21:05:36 -0400

    Update Page Admin

--- client/src/app/pages/admin-jeu-page/admin-jeu-page.component.ts
+++ client/src/app/pages/admin-jeu-page/admin-jeu-page.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-admin-jeu-page',
     templateUrl: './admin-jeu-page.component.html',
     styleUrls: ['./admin-jeu-page.component.scss'],
 })
-export class AdminJeuPageComponent {}
+export class AdminJeuPageComponent implements OnInit {
+    ngOnInit() {
+        const toggleImages = document.querySelectorAll('.toggle-image');
+
+        toggleImages.forEach((imageElement) => {
+            const image = imageElement as HTMLImageElement;
+
+            image.addEventListener('click', () => {
+                const currentSrc = image.src;
+                const altSrc = image.getAttribute('data-alt-src');
+
+                if (altSrc !== null) {
+                    image.src = altSrc;
+                    image.setAttribute('data-alt-src', currentSrc);
+                }
+            });
+        });
+    }
+}
