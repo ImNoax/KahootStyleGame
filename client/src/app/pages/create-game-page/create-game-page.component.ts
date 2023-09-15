841c469448ff4b779c1dbbb1d2e923787726f78b
Steven Ho <steven.ho01@outlook.com>
2023-09-15 18:08:45 -0400

    Déplacement des fichiers du service game-handling

--- client/src/app/pages/create-game-page/create-game-page.component.ts
+++ client/src/app/pages/create-game-page/create-game-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GameHandlingService } from '@app/game-handling.service';
+import { GameHandlingService } from '@app/services/game-handling.service';
 import { Observable } from 'rxjs';
 
 @Component({
