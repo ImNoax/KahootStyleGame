cf30654ed38fe323c58f797d75651b7c1ad4d251
Eugame <eug1214@gmail.com>
2023-09-16 17:47:54 -0400

    rebase avec le service

--- client/src/app/services/game-handling.service.spec.ts
+++ client/src/app/services/game-handling.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { GameHandlingService } from './game-handling.service';
 
 describe('GameHandlingService', () => {
-  let service: GameHandlingService;
+    let service: GameHandlingService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GameHandlingService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(GameHandlingService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
--- client/src/app/services/game-handling.service.ts
+++ client/src/app/services/game-handling.service.ts
@@ -2,14 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class GameHandlingService {
-
-    constructor(private http: HttpClient) {
-    }
+    constructor(private http: HttpClient) {}
 
     getGames() {
-        return this.http.get<{ id: number, name: string, description: string, timePerQuestion: number, questions: object }[]>('assets/jeux.json');
+        return this.http.get<{ id: number; name: string; description: string; timePerQuestion: number; questions: object }[]>('assets/jeux.json');
     }
 }
