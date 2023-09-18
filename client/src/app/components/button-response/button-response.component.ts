8cc032d27baf1affd0944660ca82b76eac05f27f
Ralph El Khoury <ralph.el-khoury@polymtl.ca>
2023-09-17 21:10:32 -0400

    ajustement css + debut logique jeu joueur

--- client/src/app/components/button-response/button-response.component.ts
+++ client/src/app/components/button-response/button-response.component.ts
@@ -5,4 +5,22 @@ import { Component } from '@angular/core';
     templateUrl: './button-response.component.html',
     styleUrls: ['./button-response.component.scss'],
 })
-export class ButtonResponseComponent {}
+export class ButtonResponseComponent {
+      buttons = [
+    { id: 1, color: 'white', clicked: false },
+    { id: 2, color: 'white', clicked: false },
+    { id: 3, color: 'white', clicked: false },
+    { id: 4, color: 'white', clicked: false }
+  ];
+
+  onButtonClick(button: { clicked: boolean; color: string; }) {
+    button.clicked = !button.clicked;
+    button.color = button.clicked ? 'lightblue' : 'white'; 
+  }
+
+  sendInfo() {
+    console.log(this.buttons);
+    console.log("test")
+   }
+
+}
