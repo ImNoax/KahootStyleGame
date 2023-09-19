4b838b8ba73e0b603c0ba981f9f0079d782a745a
Steven Ho <steven.ho01@outlook.com>
2023-09-19 19:39:06 -0400

    Test réussi de l'exportation

--- client/src/app/pages/create-game-page/create-game-page.component.ts
+++ client/src/app/pages/create-game-page/create-game-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameHandlingService } from '@app/services/game-handling.service';
 import { Jeu } from '@common/jeu';
+import { saveAs } from 'file-saver';
 
 @Component({
     selector: 'app-create-game-page',
@@ -15,6 +16,14 @@ export class CreateGamePageComponent implements OnInit {
     ngOnInit(): void {
         this.gameHandler.getGames().subscribe((games: Jeu[]) => {
             this.games = games;
+        });
+    }
+
+    fonction() {
+        return this.gameHandler.export(1).subscribe((data) => {
+            const file = new Blob([JSON.stringify(data)], { type: 'application/json' });
+            const downloadURL = window.URL.createObjectURL(file);
+            saveAs(downloadURL, "Testststs.json");
         });
     }
 }
--- client/src/app/services/game-handling.service.ts
+++ client/src/app/services/game-handling.service.ts
@@ -3,17 +3,28 @@ import { Injectable } from '@angular/core';
 import { Jeu } from '@common/jeu';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GameHandlingService {
-    private readonly baseUrl: string = 'http://localhost:3000/api';
+    private readonly baseUrl: string = environment.serverUrl;
 
     constructor(private http: HttpClient) {}
 
     getGames(): Observable<Jeu[]> {
         return this.http.get<Jeu[]>(`${this.baseUrl}/jeux`).pipe(catchError(this.handleError<Jeu[]>('getGames')));
+    }
+
+    changeVisibility(game: Jeu) {
+        return this.http
+            .patch<Jeu[]>(`${this.baseUrl}/jeux/`, { isVisible: !game.isVisible })
+            .pipe(catchError(this.handleError<Jeu[]>('changeVisibility')));
+    }
+
+    export(id: number) {
+        return this.http.get<Jeu>(`${this.baseUrl}/jeux/${id}`, { responseType: 'json' });
     }
 
     private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
