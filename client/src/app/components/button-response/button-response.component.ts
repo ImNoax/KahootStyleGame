e6fef8c468fedc0f3fec4f81bfbb0a60e319e9fb
Ralph El Khoury <ralph.el-khoury@polymtl.ca>
2023-09-18 21:39:13 -0400

    avancement logique de jeu

--- client/src/app/components/button-response/button-response.component.ts
+++ client/src/app/components/button-response/button-response.component.ts
@@ -1,26 +1,71 @@
 import { Component } from '@angular/core';
+import { GameHandlingService } from '@angular/../../client/src/app/services/game-handling.service';
+import { Jeu } from '@common/jeu';
+
+const ID_QUESTION = 1
+const ID_GAME = 0 // a transformer en service pour share le game-question id pour update 
+interface Button {
+
+  id: number;
+  color: string;
+  clicked: boolean;
+  text: string;
+  isCorrect: boolean;
+}
+
+interface Choice {
+  answer: string;
+  isCorrect: boolean;
+}
 
 @Component({
-    selector: 'app-button-response',
-    templateUrl: './button-response.component.html',
-    styleUrls: ['./button-response.component.scss'],
+  selector: 'app-button-response',
+  templateUrl: './button-response.component.html',
+  styleUrls: ['./button-response.component.scss'],
 })
-export class ButtonResponseComponent {
-      buttons = [
-    { id: 1, color: 'white', clicked: false },
-    { id: 2, color: 'white', clicked: false },
-    { id: 3, color: 'white', clicked: false },
-    { id: 4, color: 'white', clicked: false }
-  ];
 
-  onButtonClick(button: { clicked: boolean; color: string; }) {
+
+export class ButtonResponseComponent {
+  buttons: Button[] = [];
+  games: Jeu[] = [];
+
+  constructor(private gameService: GameHandlingService) { }
+
+  ngOnInit(): void {
+    this.gameService.getGames().subscribe((data: Jeu[]) => {
+      this.games = data;
+      this.updateButtons();
+    });
+  }
+
+  updateButtons() {
+    if (this.games.length) {
+      const questionOfInterest = this.games[ID_GAME].questions.find(question => question.id === ID_QUESTION);
+      if (questionOfInterest && questionOfInterest.choices) {
+        let buttonId = 1;
+        questionOfInterest.choices.forEach((choice: Choice) => {
+          this.buttons.push({
+            id: buttonId,
+            color: 'white',
+            clicked: false,
+            text: choice.answer,
+            isCorrect: choice.isCorrect
+          });
+          buttonId++;
+        });
+      }
+    }
+  }
+
+  onButtonClick(button: Button) {
     button.clicked = !button.clicked;
-    button.color = button.clicked ? 'lightblue' : 'white'; 
+    button.color = button.clicked ? 'lightblue' : 'white';
   }
 
   sendInfo() {
+
     console.log(this.buttons);
     console.log("test")
-   }
+  }
 
 }
--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { GameHandlingService } from '@angular/../../client/src/app/services/game-handling.service';
+import { Jeu } from '@common/jeu';
 
 @Component({
     selector: 'app-game-page',
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
 })
-export class GamePageComponent {}
+export class GamePageComponent {
+    games: Jeu[] = [];
+    currentQuestion: string = '';
+    constructor(private gameService: GameHandlingService) { }
+
+    ngOnInit(): void {
+        this.gameService.getGames().subscribe((data: Jeu[]) => {
+            this.games = data;
+            if (this.games.length > 0) {
+                this.currentQuestion = this.games[0].questions[0].text;
+            }
+        });
+    }
+}
