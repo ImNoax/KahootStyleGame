aa4308c3e0f46d1b261261746786c111f53219c0
Eugame <eug1214@gmail.com>
2023-09-19 00:17:10 -0400

    ajout du reactive form

--- client/src/app/services/game-handling.service.spec.ts
+++ client/src/app/services/game-handling.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GameHandlingService } from './game-handling.service';
 
 describe('GameHandlingService', () => {
     let service: GameHandlingService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
         service = TestBed.inject(GameHandlingService);
     });
 
