799302975876683ef1ceebc4f4024c7cfeecca9e
unknown <eug1214@gmail.com>
2023-09-22 10:22:19 -0400

    fix merge

--- client/src/app/components/question-creation-popup/question-creation-popup.component.ts
+++ client/src/app/components/question-creation-popup/question-creation-popup.component.ts
@@ -1,38 +1,92 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import { Question } from '@common/game';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormManagerService } from '@app/services/form-manager.service';
+import { Question, QuestionType } from '@common/jeu';
+
+const MAX_CHOICES_NUMBER = 4;
+const BASE_POINTS = 10;
 
 @Component({
     selector: 'app-question-creation-popup',
     templateUrl: './question-creation-popup.component.html',
     styleUrls: ['./question-creation-popup.component.scss'],
 })
+
 export class QuestionCreationPopupComponent implements OnInit {
     pageTitle: string = 'Liste des questions';
-    maxAnswerNumbers: number = 4; // où mettre une constante?
-    canAddAnswer: boolean;
-    isChecked: boolean = false;
 
-    answers: Question[] = [
-        // { text: 'Choix 1', type: 'Bon' },
-        // { text: 'Choix 2', type: 'Mauvais' },
-        // { text: 'Choix 3', type: 'Bon' },
-    ];
+    questionForm: FormGroup;
+    questionType: QuestionType = QuestionType.QCM;
+
+    canAddAnswer: boolean = true;
+    isChecked: boolean = true;
+
+    constructor(
+        public dialogRef: MatDialogRef<QuestionCreationPopupComponent>,
+        private fb: FormBuilder,
+        private formManager: FormManagerService
+    ) {}
 
     ngOnInit() {
-        this.canAddAnswer = this.answers.length !== this.maxAnswerNumbers;
+        this.questionForm = this.fb.group({
+            text: ['', Validators.required],
+            points: BASE_POINTS,
+            type: this.questionType,
+            choices: this.fb.array([
+                this.fb.group({
+                    answer: ['', Validators.required],
+                    isCorrect: this.isChecked
+                })
+            ])
+        });        
     }
 
-    setAnswerStyle(answer: Question) {
-        // if (answer.type === 'Bon') {
-        //     this.isChecked = true;
-        //     return { background: '#98FF7F' };
-        // }
-        // this.isChecked = false;
-        // return { background: '#FF967F' };
+    setAnswerStyle(isCorrect: boolean) {
+        if (isCorrect) {
+            this.isChecked = true;
+            return { background: '#98FF7F' };
+        }
+        this.isChecked = false;
+        return { background: '#FF967F' };
     }
 
     drop(event: CdkDragDrop<Question[]>): void {
-        moveItemInArray(this.answers, event.previousIndex, event.currentIndex);
+        moveItemInArray(this.choices.controls, event.previousIndex, event.currentIndex);
+        
+        const choices: {answer: string, isCorrect: boolean}[] = this.choices.value;
+        // Sources: https://stackoverflow.com/questions/49273499/angular-formarray-contents-order
+                    https://www.freecodecamp.org/news/swap-two-array-elements-in-javascript/
+        [choices[event.previousIndex], choices[event.currentIndex]] = [choices[event.currentIndex], choices[event.previousIndex]];
+        this.choices.setValue(choices);
+    }
+
+    get choices(): FormArray {
+        return this.questionForm.get("choices") as FormArray;
+    }
+    
+    addChoice() {
+        this.choices.push(
+            this.fb.group({
+                answer: ['', Validators.required],
+                isCorrect: true
+            })
+        );
+        this.canAddAnswer = this.choices.length !== MAX_CHOICES_NUMBER;
+    }
+
+    deleteChoice(index: number) {
+        this.choices.removeAt(index);
+        this.canAddAnswer = this.choices.length !== MAX_CHOICES_NUMBER;
+    }
+    
+    closeQuestionCreator() {
+        this.dialogRef.close();
+    }
+
+    onSubmit() {
+        this.formManager.questions.push(this.questionForm);
+        this.dialogRef.close();
     }
 }
