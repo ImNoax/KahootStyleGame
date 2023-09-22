81e983348398d4c0eec76466d94383e4f65a71b7
unknown <eug1214@gmail.com>
2023-09-22 11:26:45 -0400

    fix merge

--- common/jeu.ts
+++ common/jeu.ts
@@ -1,19 +1,19 @@
 export type Jeu = {
-    name: string,
-    description: string,
-    duration: number,
+    name: string;
+    description: string;
+    duration: number;
     questions: {
-        id: number,
-        text: string,
-        type: string,
-        points: number,
+        id: number;
+        text: string;
+        type: string;
+        points: number;
         responses?: {
-            answer: string,
-            isCorrect: boolean
-        }[],
+            answer: string;
+            isCorrect: boolean;
+        }[];
         choices?: {
-            answer: string,
-            isCorrect: boolean
-        }[]
-    }[]
-}
+            answer: string;
+            isCorrect: boolean;
+        }[];
+    }[];
+};
