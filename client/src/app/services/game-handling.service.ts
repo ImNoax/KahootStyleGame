a5e373162b3abb3846bfe1ad36951d4747e61eef
unknown <eug1214@gmail.com>
2023-09-22 11:32:13 -0400

    fix merge

--- client/src/app/services/game-handling.service.ts
+++ client/src/app/services/game-handling.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Jeu } from '@common/jeu';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
@@ -11,12 +12,22 @@ export class GameHandlingService {
     currentGameId: number = 0;
     currentQuestionId: number = 0;
 
-    private readonly baseUrl: string = 'http://localhost:3000/api';
+    private readonly baseUrl: string = environment.serverUrl;
 
     constructor(private http: HttpClient) {}
 
     getGames(): Observable<Jeu[]> {
         return this.http.get<Jeu[]>(`${this.baseUrl}/jeux`).pipe(catchError(this.handleError<Jeu[]>('getGames')));
+    }
+
+    changeVisibility(game: Jeu) {
+        return this.http
+            .patch<Jeu[]>(`${this.baseUrl}/jeux/`, { isVisible: !game.isVisible })
+            .pipe(catchError(this.handleError<Jeu[]>('changeVisibility')));
+    }
+
+    export(id: number) {
+        return this.http.get<Jeu>(`${this.baseUrl}/jeux/${id}`, { responseType: 'json' });
     }
 
     private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
