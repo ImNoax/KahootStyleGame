85ee4560a1b2db986adfcef08c0083eefcbed9a5
ImNoax <noaxnomakapro@gmail.com>
2023-10-24 17:04:58 +0000

    Waiting view Implementation

--- client/src/app/pages/waiting-view-page/waiting-view-page.component.ts
+++ client/src/app/pages/waiting-view-page/waiting-view-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ClientSocketService } from '@app/services/client-socket.service';
 
 @Component({
@@ -8,20 +9,66 @@ import { ClientSocketService } from '@app/services/client-socket.service';
 })
 export class WaitingViewPageComponent implements OnInit, OnDestroy {
     players: { socketId: string; name: string }[] = [];
-    constructor(private clientSocket: ClientSocketService) {}
+    pin: string;
+    isLocked: boolean;
+    isOrganizer: boolean;
+
+    constructor(
+        public router: Router,
+        private clientSocket: ClientSocketService,
+    ) {}
 
     ngOnInit(): void {
         this.configureBaseSocketFeatures();
         this.clientSocket.send('getPlayers');
+        this.clientSocket.send('getStatus');
     }
 
     ngOnDestroy(): void {
         this.clientSocket.canAccessLobby = false;
+        this.clientSocket.send('leaveLobby');
     }
 
     configureBaseSocketFeatures() {
-        this.clientSocket.socket.on('latestPlayerList', (players: { socketId: string; name: string }[]) => {
-            this.players = players;
+        this.clientSocket.socket.on(
+            'latestPlayerList',
+            (roomData: { pin: string; players: { socketId: string; name: string }[]; isLocked: boolean }) => {
+                this.players = roomData.players;
+                this.pin = roomData.pin;
+                this.isLocked = roomData.isLocked;
+            },
+        );
+
+        this.clientSocket.socket.on('lobbyClosed', () => {
+            this.router.navigate(['/home']);
         });
+
+        this.clientSocket.socket.on('statusOrganizer', () => {
+            this.isOrganizer = true;
+        });
+
+        this.clientSocket.socket.on('statusNotOrganizer', () => {
+            this.isOrganizer = false;
+        });
+
+        this.clientSocket.socket.on('roomLocked', () => {
+            this.isLocked = true;
+        });
+
+        this.clientSocket.socket.on('roomUnlocked', () => {
+            this.isLocked = false;
+        });
+    }
+
+    banPlayer(player: { socketId: string; name: string }) {
+        this.clientSocket.send('banPlayer', player);
+    }
+
+    lockRoom() {
+        this.clientSocket.send('lockRoom', true);
+    }
+
+    unlockRoom() {
+        this.clientSocket.send('unlockRoom', true);
     }
 }
