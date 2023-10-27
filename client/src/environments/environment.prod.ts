ea3e7ba2febe035bd8346bea141b600328594b9e
unknown <eug1214@gmail.com>
2023-10-27 11:31:35 -0400

    correction url prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    serverBaseUrl: 'http://ec2-3-99-244-22.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
-    serverGamesUrl: 'http://ec2-3-99-244-22.ca-central-1.compute.amazonaws.com:3000/api/jeux',
-    serverAdminUrl: 'http://ec2-3-99-244-22.ca-central-1.compute.amazonaws.com:3000/api/admin',
+    serverBaseUrl: 'http://ec2-99-79-33-118.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
+    serverGamesUrl: 'http://ec2-99-79-33-118.ca-central-1.compute.amazonaws.com:3000/api/jeux',
+    serverAdminUrl: 'http://ec2-99-79-33-118.ca-central-1.compute.amazonaws.com:3000/api/admin',
 };
