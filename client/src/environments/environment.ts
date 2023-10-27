07a8e7ac93038fff0145e471f2228cf6d5b09d9b
unknown <eug1214@gmail.com>
2023-10-27 10:28:43 -0400

    changement de l'url

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    serverUrl: 'http://localhost:3000/api',
+    serverUrl: 'http://ec2-3-99-244-22.ca-central-1.compute.amazonaws.com:3000/api',
 };
 
 /*
