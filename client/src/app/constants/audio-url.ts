2472a6ea6c61a03e2b975f3e98e9fa6ae1efc785
unknown <eug1214@gmail.com>
2023-11-24 11:24:42 -0500

    fix audio deploiement

--- client/src/app/constants/audio-url.ts
+++ client/src/app/constants/audio-url.ts
@@ -1,4 +1,4 @@
-const COMPONENT_AUDIO_PATH = '../../../assets/audio/';
+const COMPONENT_AUDIO_PATH = './assets/audio/';
 export const SOUNDS = ['sims4-enraged.mp3', 'sims4-hysterical.mp3', 'sims4-mortified.mp3', 'sims4-very-embarrassed.mp3', 'sims4-very-tense.mp3'];
 
 export const RAMDOM_AUDIO_URL = (sounds: string[]) => {
